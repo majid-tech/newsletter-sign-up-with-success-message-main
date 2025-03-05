d9b6cf77dc2991dabdae03ff5a2c20d689327110
Majid Mohammed <majidmohammed044@gmail.com>
2025-03-05 12:10:18 +0000

    added js file

---
+++ interactivity.js
@@ -0,0 +1,30 @@
+const successMessageContainer = document.getElementById('success-message-container');
+const dismissButton = document.getElementById('dismiss-button');
+const email = document.getElementById('email');
+const form = document.getElementById("form");
+const errorMessage = document.getElementById('error-message');
+const successMessage = document.getElementById('success-message');
+// Validate email address
+
+function validateEmail(){
+    if(email.validity.valueMissing){
+        errorMessage.textContent = 'Email required';
+    } else if(email.validity.typeMismatch){
+        errorMessage.textContent = 'valid email required';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateEmail();
+    if(email.validity.valid){
+        successMessageContainer.style.display = 'block';
+        successMessage.innerHTML = `A confirmation email has been sent to ${email.value}.
+      Please open it and click the button inside to confirm your subscription.`
+    }
+});
+
+// Hide success message when clicking dismiss button
+dismissButton.addEventListener('click', () => {
+    successMessageContainer.style.display = 'none';
+});
